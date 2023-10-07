d0315fdae973d5680e56a0a3b111935e1beacff1
rsmax <fordrsmax@gmail.com>
2023-10-07 19:03:02 +0200

    autogenerated

--- app/scripts/quiz.js
+++ app/scripts/quiz.js
@@ -1,35 +1,35 @@
-import { questions } from "./questions.js"
+import { questions,answers } from "./questions.js"
+
 
 function generateQuiz() {
 
     let form = document.querySelector("#jueguito")
 
     let fquestions = document.createElement("fieldset")
+    fquestions.setAttribute("id", "questions")
 
-    for (const q of questions) {
-        let qFieldset = document.createElement("fieldset")
-        let qlabel = document.createElement("label")
-        qlabel.setAttribute("for", q)
-        qlabel.innerHTML = q
-        qFieldset.appendChild(qlabel)
+    for (let i = 0;i<questions.length;i++) {
+        let qFieldset = document.createElement("fieldset");
+        let qlegend = document.createElement("legend");
+        qlegend.innerHTML =  questions[i];
+        qFieldset.appendChild(qlegend);
 
-        for (let i = 0; i < 4; i++) {
-            let input = document.createElement("input")
-            input.setAttribute("type", "radio")
-            input.setAttribute("name", q)
-            input.setAttribute("id", q + i)
-            let node = document.createElement("p")
-            node.innerHTML = "hola"
-            input.appendChild(node)
-            qFieldset.appendChild(input)
+        for(const a of answers[i]) {
+            let label = document.createElement("label");
+            label.setAttribute("for",a);
+            label.innerHTML = a;
+            let input = document.createElement("input");
+            input.setAttribute("type", "radio");
+            input.setAttribute("name",  questions[i]);
+            input.setAttribute("id", a);
+            qFieldset.appendChild(label);
+            qFieldset.appendChild(input);
         }
 
-        //qFieldset.setAttribute(StylePropertyMapflex")
-
-        fquestions.appendChild(qFieldset)
+        fquestions.appendChild(qFieldset);
     }
 
-    form.appendChild(fquestions)
+    form.appendChild(fquestions);
 
     let button = document.createElement("input");
     button.setAttribute("type", "submit")
